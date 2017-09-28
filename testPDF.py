913193c6f1922c935460b23ffb76d6265f620c1d
allen <allenghiggins@gmail.com>
2017-09-28 20:10:56 +0100

    read in file and out put file auto

--- testPDF.py
+++ testPDF.py
@@ -1,3 +1,13 @@
 from tabula import convert_into
+from PyPDF2 import PdfFileReader
 
-convert_into('Make_Model_data_2016.pdf', "outPut.csv", output_format="csv", pages=range(1,5))
+#filename = raw_input('Enter PDF file Name to convert to CSV (ie; myFile):-->')
+filename = 'Make_Model_data_2016' + '.pdf'
+outputFileName = filename + '.csv'
+
+try:
+    reader = PdfFileReader(filename, 'r')
+    totalPages = reader.getNumPages()
+    convert_into(filename, outputFileName, output_format="csv", pages=range(1,totalPages))
+except Exception as e:
+     print('File not found. Please check name of file or if the file has been created')
